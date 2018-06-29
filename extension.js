9267c98d4c4cbda197eb26e834c86e5677b3b7de
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-06-29 07:43:58 -0700

    Get basic icon working

--- extension.js
+++ extension.js
@@ -1,19 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
+exports.activate =
+function (context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "urdf-viewer" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    // TODO: must use `vscode.commands.executeCommand('setContext', 'flag', false);` and
+    // reference in package.json to ensure the icon only shows up when editing a URDF file
+    let disposable = vscode.commands.registerCommand('urdf-viewer.previewURDF', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
@@ -22,9 +14,7 @@ function activate(context) {
 
     context.subscriptions.push(disposable);
 }
-exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+exports.deactivate =
+function() {}
